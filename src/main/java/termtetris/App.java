72ea0aaff8a4d57eb915f794bd091d5287da6228
Misha <misha_shelemetyev@rapid7.com>
2020-07-29 10:01:42 -0400

    first commit

---
+++ src/main/java/termtetris/App.java
@@ -0,0 +1,16 @@
+/***************************************************************************
+ * COPYRIGHT (C) 2012-2020, Rapid7 LLC, Boston, MA, USA.
+ * All rights reserved. This material contains unpublished, copyrighted
+ * work including confidential and proprietary information of Rapid7.
+ **************************************************************************/
+package termtetris;
+
+public class App {
+    public String getGreeting() {
+        return "Hello world.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
---
+++ src/test/java/termtetris/AppTest.java
@@ -0,0 +1,16 @@
+/***************************************************************************
+ * COPYRIGHT (C) 2012-2020, Rapid7 LLC, Boston, MA, USA.
+ * All rights reserved. This material contains unpublished, copyrighted
+ * work including confidential and proprietary information of Rapid7.
+ **************************************************************************/
+package termtetris;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test public void testAppHasAGreeting() {
+        App classUnderTest = new App();
+        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    }
+}
